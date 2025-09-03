d3415aae3581118104ae7549ebbb421e9f7409c0
swapnilk30 <sk9137587676@gmail.com>
2025-09-03 21:16:55 +0530

    new project created 3.4.3version

---
+++ FyersBaseApp/src/main/java/com/example/fyers/FyersBaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.fyers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyersBaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FyersBaseAppApplication.class, args);
+	}
+
+}
---
+++ FyersBaseApp/src/test/java/com/example/fyers/FyersBaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fyers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FyersBaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
